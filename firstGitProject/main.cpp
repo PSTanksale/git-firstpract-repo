b0cf0ed7f614efa76fdacb0c41beddfea75289e0
z003jmcc <pooja.gulavani@siemens.com>
2023-11-01 14:50:22 +0530

    commiting main.cpp

---
+++ firstGitProject/main.cpp
@@ -0,0 +1,14 @@
+/* Author : Pooja Tanksale
+ * Date : 01/11/2023
+ * Description : This file is created to practice git. This first file on github.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << " I am On GitHub" << endl;
+	return 0;
+}
